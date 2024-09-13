47a716d0d9b0ef6214f8bdfd6cea57399a8b42b5
Ulric <ulricaird1@gmail.com>
2024-09-13 09:49:35 -0400

    idk some algorithm

--- Data Structure and Algorithms/bored/luhn algorithm.cpp
+++ Data Structure and Algorithms/bored/luhn algorithm.cpp
@@ -8,23 +8,32 @@ bool lugh_algorithm(const string &number) {
     int sum = 0;
     bool second_dig = false;
 
-    for (int i = dig; dig - 0; i--) {
+    for (int i = dig - 1; i >= 0; i--) {
         int dig_num = number[i] - '0';
 
-        if(second_dig) {
+        if (second_dig) {
             dig_num *= 2;
-
             if (dig_num > 9) {
                 dig_num -= 9;
             }
-
-            sum  += dig_num;
-            second_dig = !second_dig;
         }
 
+        sum += dig_num;
+        second_dig = !second_dig;
     } 
+
+    return (sum % 10 == 0);
 }
 
 int main() {
-    
+    cout << "Enter Your Credit Card Number" << endl;
+    string Cred_Number;
+    cin >> Cred_Number;
+
+    if (lugh_algorithm(Cred_Number)) {
+        cout << "Credit Card Number is valid" << endl;
+    } else {
+        cout << "Credit Card Number is not valid" << endl;
+    }
+
 }
