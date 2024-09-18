813a49bf99dfc48c2e9cec123778fe44079e9e13
Ulric <ulricaird1@gmail.com>
2024-09-18 04:49:42 -0400

    testing

--- Maths For IT/bored/factorial.py
+++ Maths For IT/bored/factorial.py
@@ -5,8 +5,24 @@ def factorial(n):
     print(n)
     return n * factorial(n - 1)
 
+def inverse_factorial(n):
+    x = 1
+    factorial = 1 # this is what will return if the number is a valid factorial
+
+    while (factorial < n): # this loops until the factorial variable is more than the initial value inputed
+        x += 1
+        factorial *= x
+    
+    if factorial == n: # check to see if the factorial variable that was multiplied is equal to the initial value inputed
+        return x # Return the x variable if inputed
+    else:
+        return -1 # Return -1 to indicate that the factorial variable inputed is not a valid factorial number.
+
 x = factorial(28)
 y = factorial(2) * factorial(26)
 
-z = (x / y)
+z = (x / y) # this is for a differnt
 print(z)
+
+# Inverse Factorial
+print("inverse of x and y: ", f"x: {inverse_factorial(x)}", f"y: {inverse_factorial(y)}") # the y variable outputs -1 because it is not a valid factorial number
