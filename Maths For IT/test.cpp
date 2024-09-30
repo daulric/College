2f0e0fb30887fcb4a9b2f71a849889457710e048
Ulric <ulricaird1@gmail.com>
2024-09-30 12:50:14 -0400

    idk

--- Maths For IT/test.cpp
+++ Maths For IT/test.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-#include <limits>
 
 using namespace std;
 
 void create_pyramid(int height) {
     for (int i = 1; i <= height; i++) {
-
+        // Print leading spaces
         for (int j = 0; j < height - i; j++) {
             cout << " ";
         }
-        
+        // Print hashes
         for (int j = 0; j < i; j++) {
             cout << "#";
         }
-        
         cout << endl;
     }
 }
@@ -22,21 +20,20 @@ int main() {
     int height;
 
     cout << "Enter Height: ";
-    
 
     while (true) {
         cin >> height;
 
+        // Check if the input is a valid integer
         if (cin.fail()) {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Height Must be an Integer: ";
+            cin.clear(); // Clear the error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore invalid input
+            cout << "Height must be an integer. Try again: ";
         } else if (height < 1 || height > 8) {
-            cout << "Height must be between 1 - 8: ";
+            cout << "Height must be between 1 - 8. Try again: ";
         } else {
-            break;
+            break; // Valid integer input in the correct range
         }
-
     }
 
     create_pyramid(height);
