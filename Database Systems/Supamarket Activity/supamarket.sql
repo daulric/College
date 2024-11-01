a143eff750e305fcd34e1d6f2a0cf03415d5a859
Ulric <ulricaird1@gmail.com>
2024-10-31 20:21:38 -0400

    learning sql with vscode

--- Database Systems/Supamarket Activity/supamarket.sql
+++ Database Systems/Supamarket Activity/supamarket.sql
@@ -3,6 +3,11 @@ CREATE TABLE Department (
     DepartmentName VARCHAR(255),
     PRIMARY KEY (DeptID)
 );
+
+INSERT INTO Department (DeptID, DepartmentName)
+VALUES ('HR', 'Human Resource')
+('FIN', 'Finance') ('MK', 'Marketing') ('DLV', "Delivery");
+
 CREATE TABLE Employee (
     EmployeeID VARCHAR(18) NOT NULL UNIQUE,
     EmployeeName VARCHAR(255),
@@ -11,6 +16,14 @@ CREATE TABLE Employee (
     PRIMARY KEY (EmployeeID),
     FOREIGN KEY (DeptID) REFERENCES Department(DeptID)
 );
+
+INSERT INTO Employee (EmployeeID, EmployeeName, PhoneNumber, DeptID)
+VALUES ('EMP1', 'Ulric Aird', "478-0921", "HR100")
+("EMP2", "Ted Bundy", "982-8743", "FIN")
+("EMP3", "Kyle Richh", "837-1920", "FIN")
+("EMP4", "Anthony Higgins", "873-9293", "MK")
+("EMP5", "Mya Khalifa", "892-2039", "DLV");
+
 CREATE TABLE Supplier (
     SupplierID VARCHAR(18) NOT NULL UNIQUE,
     SupplierName VARCHAR(255),
@@ -19,6 +32,12 @@ CREATE TABLE Supplier (
     PRIMARY KEY (SupplierID),
     FOREIGN KEY (DeptID) REFERENCES Department(DeptID)
 );
+
+INSERT INTO Supplier (SupplierID, SupplierName, ContactNumber, DeptID)
+VALUES ("IAL", "Independent Agency Limited", "672-9374", "DLV")
+("SUB", "Superb Distributor", "928-3920", "DLV")
+("HuD", "Hubbard Distributors", "823-2083", "DLV");
+
 CREATE TABLE Product (
     ProductID VARCHAR(18) NOT NULL UNIQUE,
     ProductName VARCHAR(255),
