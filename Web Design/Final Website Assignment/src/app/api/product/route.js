425439d41e01a2e13d7f19240b18b08a78ee449d
Ulric <ulricaird1@gmail.com>
2024-11-27 18:15:25 -0400

    Changes

--- Web Design/Final Website Assignment/src/app/api/product/route.js
+++ Web Design/Final Website Assignment/src/app/api/product/route.js
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client"
-import { unstable_noStore } from "next/cache";
 
 export async function GET() {
-    unstable_noStore();
     const client = new PrismaClient();
     
     const items = await client.product.findMany({
