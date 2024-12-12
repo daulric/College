4b79ed2633ea5ebd8c1e59185f8e66390b17fc74
Ulric Aird <ulricaird1@gmail.com>
2024-12-12 10:09:52 -0400

    Added some changes to this repo

---
+++ Maths For IT/functions/fibonnaci.lua
@@ -0,0 +1,7 @@
+local function fibonacci(x)
+    if (x <= 0) then return 0 end;
+    if (x == 1) then return 1 end;
+    return fibonacci(x - 2) + fibonacci(x - 1);
+end
+
+print(fibonacci(2));
---
+++ Maths For IT/functions/index.lua
@@ -0,0 +1,6 @@
+function f(x)
+    return x^2  + 2*x + 7;
+end
+
+local number = tonumber(io.read());
+print(f(number));
