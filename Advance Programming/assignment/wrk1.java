86237d82171ccb5852ab54a49d589bbd9615dab0
Ulric Aird <ulricaird1@gmail.com>
2025-01-28 11:27:22 -0400

    new materials

---
+++ Advance Programming/assignment/wrk1.java
@@ -0,0 +1,32 @@
+package assignment;
+
+import java.util.*;;
+
+public class wrk1 { //Class Here
+
+    public static void displayHighScorePosition(String playerName, int playerPosition) {
+        System.out.println(playerName + " managed to get into position " + playerPosition + " on the high score list");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) return 1;
+        if  (playerScore >= 500 && playerScore < 1000) return 2;
+        if (playerScore >= 100 && playerScore < 500) return 3;
+        return 4;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Player Name: ");
+        String playerName = s.next();
+        System.out.print("Enter Player Position: ");
+        int playerScore = s.nextInt();
+
+        displayHighScorePosition(playerName, playerScore);
+
+        System.out.print("Enter Highscore: ");
+        int number = s.nextInt();
+        System.out.println(calculateHighScorePosition(number));
+        s.close();
+    }
+}
